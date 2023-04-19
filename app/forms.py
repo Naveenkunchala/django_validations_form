b1b032ef303e4b0a9e6f900835cb3574d76ef979
kunchalanaveen <kunchalanaveen80@gmail.com>
2023-04-19 14:42:01 +0530

    first commit

---
+++ app/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+g=[('MALE','male'),('FEMALE','female')]
+c=[['PYTHON','python'],('JAVA','java'),['SQL','sql']]
+
+
+class studentForm(forms.Form):
+    name=forms.CharField(max_length=100)
+    age=forms.IntegerField()
+    url=forms.URLField()
+    email=forms.EmailField()
+    date=forms.DateTimeField()
+    password=forms.CharField(max_length=100,widget=forms.PasswordInput)
+    address=forms.CharField(max_length=100,widget=forms.Textarea)
+    #gender=forms.ChoiceField(choices=g)
+    gender=forms.ChoiceField(choices=g,widget=forms.RadioSelect)
+    #course=forms.MultipleChoiceField(choices=c)
+    course=forms.MultipleChoiceField(choices=c,widget=forms.CheckboxSelectMultiple)
+    
+    
+    
