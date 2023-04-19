57cb43f7f60c2248fba838bbb850743c94499a30
kunchalanaveen <kunchalanaveen80@gmail.com>
2023-04-19 15:01:55 +0530

    first commit

--- app/forms.py
+++ app/forms.py
@@ -10,7 +10,7 @@ class studentForm(forms.Form):
     email=forms.EmailField()
     date=forms.DateTimeField()
     password=forms.CharField(max_length=100,widget=forms.PasswordInput)
-    address=forms.CharField(max_length=100,widget=forms.Textarea)
+    address=forms.CharField(max_length=100,widget=forms.Textarea(attrs={'cols':10,'rows':10}))
     #gender=forms.ChoiceField(choices=g)
     gender=forms.ChoiceField(choices=g,widget=forms.RadioSelect)
     #course=forms.MultipleChoiceField(choices=c)
